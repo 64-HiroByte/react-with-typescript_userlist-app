fbcd7447944b7424c24dd1ab319e2500a87ad87f
64-HiroByte <higher.ground.rc@gmail.com>
2025-10-30 12:12:40 +0900

    App.tsxで表示切り替え用のstateを定義した

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import { AllUserList } from "./components/AllUserList";
 
 function App() {
+  const [view, setView] = useState<"all" | "student" | "mentor">("all");
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <AllUserList />
+      <div>
+        <button onClick={() => setView("all")}>全ユーザー</button>
+        <button onClick={() => setView("student")}>生徒</button>
+        <button onClick={() => setView("mentor")}>メンター</button>
+      </div>
+      {view === "all" && <AllUserList />}
+      {/* {view === "all" && <StudentList />} */}
+      {/* {view === "all" && <MentorList />} */}
     </>
   );
 }
