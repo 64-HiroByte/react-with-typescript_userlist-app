90e16e401fccb3efa388722f1f609afeb47da129
64-HiroByte <higher.ground.rc@gmail.com>
2025-10-30 17:19:01 +0900

    生徒用データ、メンターデータをコンポーネント分割した

---
+++ src/components/cell/StudentDataCell.tsx
@@ -0,0 +1,25 @@
+import type { FC } from "react";
+
+import type { UserType } from "../../types/user";
+import { TABLE_BORDER, TB_PRELINE } from "../../styles/style";
+import { isStudentUser } from "../../utils/typeGuards";
+import { findSupportedUsers } from "../../utils/findSupportedUsers";
+import { USER_LIST } from "../../data/userList";
+
+export const StudentDataCell: FC<{ user: UserType }> = ({ user }) => {
+  const supportedUsers = findSupportedUsers(user, USER_LIST);
+
+  if (!isStudentUser(user)) return null;
+
+  return (
+    <>
+      <td className={TABLE_BORDER}>{user.studyMinutes}</td>
+      <td className={TABLE_BORDER}>{user.taskCode}</td>
+      <td className={TB_PRELINE}>{user.studyLangs.join("\n")}</td>
+      <td className={TABLE_BORDER}>{user.score}</td>
+      <td className={TB_PRELINE}>
+        {supportedUsers.map((u) => u.name).join("\n ")}
+      </td>
+    </>
+  );
+};
--- src/components/row/UserRow.tsx
+++ src/components/row/UserRow.tsx
@@ -1,33 +1,25 @@
 import type { FC } from "react";
 
-import { USER_LIST } from "../../data/userList";
-import { findSupportedUsers } from "../../utils/findSupportedUsers";
 import { BaseDataCell } from "../cell/BaseDataCell";
 
 import type { UserType } from "../../types/user";
 
-import { TABLE_BORDER, TB_PRELINE } from "../../styles/style";
+import { TABLE_BORDER } from "../../styles/style";
 import { isStudentUser } from "../../utils/typeGuards";
+import { StudentDataCell } from "../cell/StudentDataCell";
+import { MentorDataCell } from "../cell/MentorDataCell";
 
 export const UserRow: FC<{ user: UserType }> = ({ user }) => {
-  const supportedUsers = findSupportedUsers(user, USER_LIST);
-
   const STUDENT_EMPTY_COLS = 5;
   const MENTOR_EMPTY_COLS = 5;
 
+  // 生徒の場合
   if (isStudentUser(user)) {
-    // 生徒の場合
     return (
       <tr className={TABLE_BORDER}>
         <BaseDataCell user={user} />
         {/* Student */}
-        <td className={TABLE_BORDER}>{user.studyMinutes}</td>
-        <td className={TABLE_BORDER}>{user.taskCode}</td>
-        <td className={TB_PRELINE}>{user.studyLangs.join("\n")}</td>
-        <td className={TABLE_BORDER}>{user.score}</td>
-        <td className={TB_PRELINE}>
-          {supportedUsers.map((u) => u.name).join("\n ")}
-        </td>
+        <StudentDataCell user={user} />
         {/* Mentorに関するセルは空欄（5個） */}
         {renderEmptyCells(STUDENT_EMPTY_COLS, user.id)}
       </tr>
@@ -41,13 +33,7 @@ export const UserRow: FC<{ user: UserType }> = ({ user }) => {
       {/* Studentに関するデータは空欄（5個） */}
       {renderEmptyCells(MENTOR_EMPTY_COLS, user.id)}
       {/* Mentor */}
-      <td className={TABLE_BORDER}>{user.experienceDays}</td>
-      <td className={TB_PRELINE}>{user.useLangs.join("\n")}</td>
-      <td className={TABLE_BORDER}>{user.availableStartCode}</td>
-      <td className={TABLE_BORDER}>{user.availableEndCode}</td>
-      <td className={TB_PRELINE}>
-        {supportedUsers.map((u) => u.name).join("\n")}
-      </td>
+      <MentorDataCell user={user} />
     </tr>
   );
 };
