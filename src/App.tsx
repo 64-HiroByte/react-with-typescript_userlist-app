029ef03f769ebe8ef76558ca5a6fa551fa27ea2e
64-HiroByte <higher.ground.rc@gmail.com>
2025-11-02 15:50:58 +0900

    ユーザー一覧とモーダル表示を管理するstateを定義した

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
+
 import { UserTableContainer } from "./components/organisms/UserTableContainer";
+import type { ViewType } from "./types/table";
+import type { UserType } from "./types/user";
+import { USER_LIST } from "./data/userList";
 
 function App() {
-  const [view, setView] = useState<"all" | "student" | "mentor">("all");
+  const [view, setView] = useState<ViewType>("all");
+  const [users, setUsers] = useState<UserType[]>(USER_LIST);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
@@ -11,10 +17,13 @@ function App() {
         <button onClick={() => setView("all")}>全ユーザー</button>
         <button onClick={() => setView("student")}>生徒</button>
         <button onClick={() => setView("mentor")}>メンター</button>
+        <button onClick={() => setIsModalOpen(true)}>新規追加</button>
       </div>
 
       {/* データテーブル描画 */}
-      <UserTableContainer view={view} />
+      <UserTableContainer view={view} users={users} />
+
+      {/* {if (isModalOpen) && <AddUserModal />} */}
     </>
   );
 }
